2bb3075cee1273ffc66b6fa5915741102e7f6529
KreativeThinker <sagely2005eagle@gmail.com>
2025-04-02 19:43:28 +0530

    chore: skip more frames to lower computation

--- dynago/config.py
+++ dynago/config.py
@@ -4,6 +4,6 @@ with open("dynago/data/gesture_map.json", "r") as file:
     GESTURE_MAP = json.load(file)
     GESTURE_MAP = {int(k): v for k, v in GESTURE_MAP.items()}
 
-N_FRAMES = 2  # Static gesture classification every N frames
+N_FRAMES = 6  # Static gesture classification every N frames
 VEL_THRESHOLD = 0.25  # Minimum movement for swipe detection
 DISPLAY_TIME = 1  # Seconds to display swipe direction
