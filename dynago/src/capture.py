be449bc36d19c63ffd2ccab34d01ee08b751aee1
KreativeThinker <sagely2005eagle@gmail.com>
2025-04-05 16:41:44 +0530

    chore: Increase tracking confidence

--- dynago/src/capture.py
+++ dynago/src/capture.py
@@ -137,7 +137,7 @@ def capture_landmarks(cmd_queue):
         static_image_mode=False,
         max_num_hands=1,
         min_detection_confidence=0.7,
-        min_tracking_confidence=0.5,
+        min_tracking_confidence=0.7,
     ) as hands:
 
         while cap.isOpened():
